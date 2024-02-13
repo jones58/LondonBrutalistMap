7614026c5d707c0e3803f10186a871c69b32ba1f
jones58 <108466184+jones58@users.noreply.github.com>
2024-02-13 14:06:35 +0000

    edit about page down

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -32,50 +32,6 @@ export default async function About() {
             most striking civic and housing projects ever built.
           </p>
         </section>
-        <section>
-          <h2 className="text-2xl font-bold">Building Index</h2>
-          <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-            {features.map((feature: any) => (
-              <li
-                key={feature.id}
-                className="w-full sm:w-[400px] sm:p-4"
-              >
-                <div className="flex flex-col items-center justify-center bg-gray-100 rounded-lg overflow-hidden">
-                  <Image
-                    src={feature.properties.Image}
-                    alt={feature.properties.Title}
-                    width={400}
-                    height={400}
-                    objectFit="cover"
-                    sizes="100vw"
-                    className="filter grayscale w-full w-[400px] h-[400px]"
-                  />
-                  <div className="p-4 w-full h-40">
-                    <h1 className="text-xl font-bold mb-2">
-                      {feature.properties.Title}
-                    </h1>
-                    <div className="text-sm mb-2">
-                      <span className="font-bold">Designed by:</span>{" "}
-                      {feature.properties.Designer}
-                    </div>
-                    <div className="text-sm mb-2">
-                      <span className="font-bold">Completed in:</span>{" "}
-                      {feature.properties.Completed}
-                    </div>
-                    <a
-                      href={feature.properties.URL}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-sm font-bold hover:underline"
-                    >
-                      View on Map
-                    </a>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </section>
       </div>
     </main>
   );

ec5a96f017178cfc30b7e78c89108b251d96ac77
jones58 <108466184+jones58@users.noreply.github.com>
2024-02-13 15:14:26 +0000

    add form to game (needs debugging)

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,5 +1,6 @@
 import { promises as fs } from "fs";
 import Image from "next/image";
+import Form from "../components/form";
 
 export default async function About() {
   const file = await fs.readFile(
@@ -9,46 +10,31 @@ export default async function About() {
   const data = JSON.parse(file);
   const features = data.features;
 
+  // Pick a random building on launch //
+  const feature =
+    features[Math.floor(Math.random() * features.length)];
+
+  const featureTitle = feature.properties.Title;
+
   return (
-    <div>{features.length}</div>
-    /*
-          <li key={feature.id} className="w-full sm:w-[400px] sm:p-4">
-            <div className="flex flex-col items-center justify-center bg-gray-100 rounded-lg overflow-hidden">
-              <Image
-                src={feature.properties.Image}
-                alt={feature.properties.Title}
-                width={400}
-                height={400}
-                objectFit="cover"
-                sizes="100vw"
-                className="filter grayscale w-full w-[400px] h-[400px]"
-              />
-              <div className="p-4 w-full h-40">
-                <h1 className="text-xl font-bold mb-2">
-                  {feature.properties.Title}
-                </h1>
-                <div className="text-sm mb-2">
-                  <span className="font-bold">Designed by:</span>{" "}
-                  {feature.properties.Designer}
-                </div>
-                <div className="text-sm mb-2">
-                  <span className="font-bold">Completed in:</span>{" "}
-                  {feature.properties.Completed}
-                </div>
-                <a
-                  href={feature.properties.URL}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-sm font-bold hover:underline"
-                >
-                  View on Map
-                </a>
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </section>
-    */
+    <div className="w-full sm:w-[400px] sm:p-4">
+      <div className="flex flex-col items-center justify-center bg-gray-100 rounded-lg overflow-hidden">
+        <Image
+          alt="A brutalist building in black and white"
+          src={feature.properties.Image}
+          width={400}
+          height={400}
+          objectFit="cover"
+          sizes="100vw"
+          className="filter grayscale w-full w-[400px] h-[400px]"
+        />
+        <div className="p-4 w-full h-40">
+          <h1 className="text-xl font-bold mb-2">
+            What building is this?
+          </h1>
+          <Form featureTitle={featureTitle.featureTitle} />
+        </div>
+      </div>
+    </div>
   );
 }

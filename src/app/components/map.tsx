4e8a9a3c203d9dac387e1fac4dc2807356f2f8af
jones58 <108466184+jones58@users.noreply.github.com>
2024-02-16 16:10:55 +0000

    edit map

--- src/app/components/map.tsx
+++ src/app/components/map.tsx
@@ -1,24 +1,29 @@
 "use client";
-import Map from 'react-map-gl';
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { useEffect, useState } from "react";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 export default function MapboxMap() {
   return (
     <div className="w-full">
-    <Map
-      mapboxAccessToken= "pk.eyJ1Ijoiam9uZXM1ODEiLCJhIjoiY2xwNzAyeDVlMXlxZzJqbzVvcnBjbDZ3OCJ9.cuGt6LfugBHuqw9rJKQ2DA"
-      initialViewState={{
-        longitude: -0.1276,
-        latitude: 51.5074,
-        zoom: 9
-      }}
-
-      style={{width: "100%", height: "70vh"}}
-
-      mapStyle="mapbox://styles/jones581/clp8fym2g01u901qmbpzq0dde"
-    />
+      <ReactMapGL
+        mapboxAccessToken="pk.eyJ1Ijoiam9uZXM1ODEiLCJhIjoiY2xwNzAyeDVlMXlxZzJqbzVvcnBjbDZ3OCJ9.cuGt6LfugBHuqw9rJKQ2DA"
+        initialViewState={{
+          longitude: -0.1276,
+          latitude: 51.5074,
+          zoom: 10,
+        }}
+        style={{ width: "100%", height: "70vh" }}
+        mapStyle="mapbox://styles/jones581/clp8fym2g01u901qmbpzq0dde"
+      >
+        {/* features.map((feature: any) => (
+          <Marker
+            key={feature.id}
+            longitude={feature.geometry.coordinates[0]}
+            latitude={feature.geometry.coordinates[1]}
+          ></Marker>
+         ))*/}
+      </ReactMapGL>
     </div>
-
-
   );
 }

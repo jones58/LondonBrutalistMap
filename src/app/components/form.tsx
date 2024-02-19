1800cafbcaffda79a3ec4e1293c9e4297395b45d
jones58 <108466184+jones58@users.noreply.github.com>
2024-02-19 17:34:49 +0000

    editing game logic

--- src/app/components/form.tsx
+++ src/app/components/form.tsx
@@ -1,23 +1,29 @@
 "use client";
 import { FormEvent } from "react";
 
-function Form(featureTitle: any) {
+interface Feature {
+  featureTitle: string;
+}
+
+function Form({ featureTitle }: Feature) {
   return (
     <form
       onSubmit={(event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const formData = new FormData(event.currentTarget);
-        const guess = formData.get("guess");
+        const guess: string = (formData.get("guess") || "")
+          .toString()
+          .trim();
         console.log("guess is:");
         console.log(guess);
         console.log("building is actually:");
         console.log(featureTitle);
         console.log("submitted value");
-        if (
-          featureTitle.featureTitle
-            .toLowerCase()
-            .includes(guess.toLowerCase())
-        ) {
+        if (guess === "") {
+          alert("Please enter a guess.");
+          return;
+        }
+        if (featureTitle.toLowerCase().includes(guess.toLowerCase())) {
           alert("correct");
           window.location.reload();
         } else {

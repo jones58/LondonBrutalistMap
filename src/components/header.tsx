fcc3e2145f5b8453face68a6a7a705ef7a6e759b
jones58 <108466184+jones58@users.noreply.github.com>
2024-02-22 17:00:10 +0000

    update header height and map height

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div className="w-full h-[20vh] bg-white text-black text-2xl flex flex-col justify-center sm:flex-row sm:justify-between space-x-4 py-[1vh] place-items-center text-left">
+    <div className="w-full h-[15vh] bg-white text-black text-2xl flex flex-col justify-center sm:flex-row sm:justify-between space-x-4 py-[1vh] place-items-center text-left">
       <Link to="/">
         <h1 className="text-3xl font-bold">London Brutalist Map</h1>
       </Link>
--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import { useState, useCallback } from "react";
 import Map, {
   Marker,
   GeolocateControl,
@@ -9,28 +9,22 @@ import Map, {
 import { FeaturesData } from "../components/geoJSONData.tsx";
 
 export default function MapPage() {
-  const [selectedMarker, setSelectedMarker] = useState(null);
+  const [clickedMarker, setClickedMarker] = useState(null);
   const [hoveredMarker, setHoveredMarker] = useState(null);
 
   const features = FeaturesData.features;
 
   const handleClick = useCallback((feature) => {
-    setSelectedMarker(feature.properties);
-    console.log(`clicked: ${feature.properties.Title}`);
+    setClickedMarker(feature);
   }, []);
 
   const handleHover = useCallback((feature) => {
     setHoveredMarker(feature);
-    alert("hovered");
   }, []);
-
-  const handleMouseEnter = () => {
-    alert("hovered");
-  };
 
   return (
     <div>
-      <div className="w-full">
+      <div className="w-full h-[80vh]">
         <Map
           mapboxAccessToken="pk.eyJ1Ijoiam9uZXM1ODEiLCJhIjoiY2xwNzAyeDVlMXlxZzJqbzVvcnBjbDZ3OCJ9.cuGt6LfugBHuqw9rJKQ2DA"
           initialViewState={{
@@ -38,7 +32,7 @@ export default function MapPage() {
             latitude: 51.5074,
             zoom: 10,
           }}
-          style={{ width: "100%", height: "70vh" }}
+          style={{ width: "100%", height: "75vh" }}
           mapStyle="mapbox://styles/jones581/clp8fym2g01u901qmbpzq0dde"
         >
           <GeolocateControl position="top-left" />
@@ -50,22 +44,25 @@ export default function MapPage() {
               key={feature.id}
               longitude={feature.geometry.coordinates[0]}
               latitude={feature.geometry.coordinates[1]}
-              onClick={(feature) => handleClick(feature)}
-              onHover={(feature) => handleHover(feature)}
-              onMouseEnter={handleMouseEnter}
             >
               <svg
+                onMouseEnter={() => handleHover(feature)}
+                onMouseLeave={() => setHoveredMarker(null)}
                 xmlns="http://www.w3.org/2000/svg"
                 width="20"
                 height="20"
                 viewBox="0 0 20 20"
-                fill="#D81E5B"
+                fill={
+                  localStorage.getItem(feature.id) === "true"
+                    ? "#00BD9D"
+                    : "#D81E5B"
+                }
               >
                 <circle cx="10" cy="10" r="6" />
               </svg>
             </Marker>
           ))}
-          {selectedMarker && <div>HELLOOO</div>}
+          {clickedMarker && <div>HELLOOO</div>}
           {hoveredMarker && <div>HELLOOO2</div>}
         </Map>
       </div>

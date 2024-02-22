1d34c8888f35c8f9d0a887bbf8967a37509dc6ad
jones58 <108466184+jones58@users.noreply.github.com>
2024-02-22 12:44:51 +0000

    added markers to map

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,4 +1,4 @@
-import Map from "react-map-gl";
+import Map, { Marker } from "react-map-gl";
 import { FeaturesData } from "../components/geoJSONData.tsx";
 
 export default function MapPage() {
@@ -14,7 +14,25 @@ export default function MapPage() {
           }}
           style={{ width: "100%", height: "70vh" }}
           mapStyle="mapbox://styles/jones581/clp8fym2g01u901qmbpzq0dde"
-        />
+        >
+          {FeaturesData.features.map((feature: any) => (
+            <Marker
+              key={feature.id}
+              longitude={feature.geometry.coordinates[0]}
+              latitude={feature.geometry.coordinates[1]}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                fill="red"
+              >
+                <circle cx="10" cy="10" r="6" />
+              </svg>
+            </Marker>
+          ))}
+        </Map>
       </div>
     </div>
   );

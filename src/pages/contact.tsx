90827473935c8f76a2a67ffa465b41055c78b610
jones58 <108466184+jones58@users.noreply.github.com>
2024-02-22 17:21:42 +0000

    edit contact form look

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -12,55 +12,53 @@ export default function Contact() {
   };
 
   return (
-    <main>
-      <div className="overflow-x-hidden flex justify-center">
-        <form
-          className="max-w-md w-full bg-black p-10 rounded-lg shadow-lg space-y-5"
-          onSubmit={handleSubmit}
+    <div className="overflow-x-hidden flex justify-center ">
+      <form
+        className="max-w-md w-full bg-black p-7 pb-8 rounded-lg shadow-lg"
+        onSubmit={handleSubmit}
+      >
+        <h1 className="text-2xl font-bold text-white">
+          Have a suggestion? Feel free to reach out here.
+        </h1>
+        <label className="text-white mb-4 block">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="mt-1 block w-full rounded-md bg-gray-100 text-black border-gray-600 p-5"
+          />
+        </label>
+        <label className="text-white mb-4 block">
+          Email
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="mt-1 block w-full rounded-md bg-gray-100 text-black border-gray-600 p-5"
+          />
+        </label>
+        <label className="text-white mb-4 block">
+          Message
+          <textarea
+            id="message"
+            name="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            className="mt-1 block w-full rounded-md bg-gray-100 text-black border-gray-600 p-7"
+          ></textarea>
+        </label>
+        <button
+          type="submit"
+          className="text-center text-2xl w-full bg-white font-bold text-left text-black p-10 rounded-md hover:bg-gray-200"
         >
-          <h1 className="text-2xl font-bold text-white">
-            Have a suggestion? Feel free to reach out here.
-          </h1>
-          <label className="text-white mb-4 block">
-            Name
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="mt-1 block w-full rounded-md bg-gray-100 text-black border-gray-600 p-5"
-            />
-          </label>
-          <label className="text-white mb-4 block">
-            Email
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="mt-1 block w-full rounded-md bg-gray-100 text-black border-gray-600 p-5"
-            />
-          </label>
-          <label className="text-white mb-4 block">
-            Message
-            <textarea
-              id="message"
-              name="message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              className="mt-1 block w-full rounded-md bg-gray-100 text-black border-gray-600 p-7"
-            ></textarea>
-          </label>
-          <button
-            type="submit"
-            className="text-center text-2xl w-full bg-white font-bold text-left text-black p-10 rounded-md hover:bg-gray-200"
-          >
-            Submit
-          </button>
-        </form>
-      </div>
-    </main>
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }

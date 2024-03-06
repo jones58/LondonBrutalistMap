bcd382688465498e27fc7e655a976db55c6a77ba
jones58 <108466184+jones58@users.noreply.github.com>
2024-03-06 14:11:40 +0000

    spacing bottom of page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { FeaturesData } from "../components/geoJSONData.tsx";
 export default function Index() {
   const features = FeaturesData.features;
   return (
-    <section className="overflow-x-hidden">
+    <section className="overflow-x-hidden pb-10 mb-10">
       <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {features.map((feature: any) => (
           <li key={feature.id} className="w-full sm:w-[400px] sm:p-4">

91e9e4290637d9a38ecc6bd7345087cece906628
jones58 <108466184+jones58@users.noreply.github.com>
2024-03-18 18:28:46 +0000

    fix index page manipulating local storage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,57 +15,72 @@ export default function Index() {
     setVisitedStates(updatedVisitedStates);
   };
 
+  const isVisited = (title: string) => {
+    return localStorage.getItem(title);
+  };
+
+  const toggleVisited = (title: string) => {
+    if (isVisited(title)) {
+      localStorage.removeItem(title);
+    } else {
+      localStorage.setItem(title, "visited");
+    }
+  };
+
   return (
     <section className="overflow-x-hidden pb-10 mb-10">
       <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {features.map((feature, index) => (
-          <li key={feature.id} className="w-full sm:w-[400px] sm:p-4">
-            <div className="flex flex-col items-center justify-center bg-gray-100 rounded-lg overflow-hidden">
-              <img
-                src={feature.properties.Image}
-                alt={feature.properties.Title}
-                width={400}
-                height={400}
-                sizes="100vw"
-                className="filter grayscale w-full w-[400px] h-[400px] object-cover"
-              />
-              <div className="p-4 w-full h-40">
-                <h1 className="text-xl font-bold mb-2">
-                  {feature.properties.Title}
-                </h1>
-                <div className="text-sm mb-2">
-                  <span className="font-bold">Designed by:</span>{" "}
-                  {feature.properties.Designer}
-                </div>
-                <div className="text-sm mb-2">
-                  <span className="font-bold">Completed in:</span>{" "}
-                  {feature.properties.Completed}
-                </div>
-                <a
-                  href={feature.properties.URL}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-sm font-bold hover:underline"
-                >
-                  Get Directions
-                </a>
-              </div>
-            </div>
-            <div
-              className="flex flex-col justify-between left-[270px] relative w-20 h-20 bottom-[250px]  bg-white rounded-lg"
-              onClick={() => handleVisitedToggle(index)}
+        {features.map((feature, index) => {
+          const title = feature.properties.Title;
+          const visited = isVisited(title);
+          return (
+            <li
+              key={feature.id}
+              className="w-full sm:w-[400px] sm:p-4"
             >
-              {visitedStates[index] && (
-                <VisitedCheckbox className="pt-3" />
-              )}
-              {!visitedStates[index] && (
-                <UnvisitedCheckbox className="pt-3" />
-              )}
-              <p className="text-center">Visited?</p>
-            </div>
-            )
-          </li>
-        ))}
+              <div className="flex flex-col items-center justify-center bg-gray-100 rounded-lg overflow-hidden">
+                <img
+                  src={feature.properties.Image}
+                  alt={title}
+                  width={400}
+                  height={400}
+                  sizes="100vw"
+                  className="filter grayscale w-full w-[400px] h-[400px] object-cover"
+                />
+                <div className="p-4 w-full h-40">
+                  <h1 className="text-xl font-bold mb-2">{title}</h1>
+                  <div className="text-sm mb-2">
+                    <span className="font-bold">Designed by:</span>{" "}
+                    {feature.properties.Designer}
+                  </div>
+                  <div className="text-sm mb-2">
+                    <span className="font-bold">Completed in:</span>{" "}
+                    {feature.properties.Completed}
+                  </div>
+                  <a
+                    href={feature.properties.URL}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-sm font-bold hover:underline"
+                  >
+                    Get Directions
+                  </a>
+                </div>
+              </div>
+              <div
+                className="flex flex-col justify-between left-[270px] relative w-20 h-20 bottom-[250px] bg-white rounded-lg"
+                onClick={() => {
+                  toggleVisited(title);
+                  handleVisitedToggle(index);
+                }}
+              >
+                {visited && <VisitedCheckbox className="pt-3" />}
+                {!visited && <UnvisitedCheckbox className="pt-3" />}
+                <p className="text-center">Visited?</p>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </section>
   );

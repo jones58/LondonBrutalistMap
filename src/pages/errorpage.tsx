99c095b9f988271e898d481627ece75ef78edf93
jones58 <108466184+jones58@users.noreply.github.com>
2024-08-27 17:58:05 +0100

    edit error 404 page

--- src/pages/errorpage.tsx
+++ src/pages/errorpage.tsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 export default function ErrorPage() {
   return (
     <div className="flex flex-col gap-4">
-      <h1 className="text-2xl font-bold">Page Not Found</h1>
-      <Link to="/">Back to Home</Link>
+      <h1 className="text-2xl font-bold ">Page Not Found</h1>
+      <Link to="/">
+        <p className="hover:underline">Back to Home</p>
+      </Link>
     </div>
   );
 }

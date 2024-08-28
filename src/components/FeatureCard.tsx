0d00d4ced2105d651487e12f8f7d94e723c3ab4d
jones58 <108466184+jones58@users.noreply.github.com>
2024-08-28 15:58:01 +0100

    extract common tailwind styles to scss

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -1,7 +1,7 @@
-import React from "react";
 import VisitedCheckbox from "./visitedCheckbox";
 import UnvisitedCheckbox from "./unvisitedCheckbox";
 import CloseButton from "./closeButton";
+import "./FeatureCard.scss";
 
 interface FeatureCardProps {
   feature: GeoJSON.Feature;
@@ -21,53 +21,48 @@ const FeatureCard: React.FC<FeatureCardProps> = ({
   if (!isOpen) return null;
 
   return (
-    <div className="relative flex flex-col items-center justify-center bg-gray-100 sm:rounded-lg overflow-hidden">
+    <div className="feature-card__container">
       <img
         src={`./images/buildings/${feature.properties?.Image}.webp`}
         alt={feature.properties?.Title}
         width={400}
         height={400}
         sizes="100vw"
-        className="filter grayscale w-full w-[400px] h-[400px] object-cover"
+        className="feature-card__image"
       />
-      <div className="p-4 w-full h-40">
-        <h1 className="text-xl font-bold mb-2">
+
+      <div className="feature-card__content">
+        <h1 className="feature-card__title">
           {feature.properties?.Title}
         </h1>
-        <div className="text-sm mb-2">
-          <span className="">Designed by:</span>{" "}
-          {feature.properties?.Designer}
+        <div className="feature-card__info">
+          <span>Designed by:</span> {feature.properties?.Designer}
         </div>
-        <div className="text-sm mb-2">
-          <span className=" text-red">Completed in:</span>{" "}
+
+        <div className="feature-card__info">
+          <span className="text-red">Completed in:</span>{" "}
           {feature.properties?.Completed}
         </div>
         <a
           href={feature.properties?.URL}
           target="_blank"
           rel="noopener noreferrer"
-          className="text-sm  hover:underline"
+          className="feature-card__link"
         >
           Get Directions
         </a>
       </div>
-
-      <div className="absolute bottom-2 right-2 flex space-x-4">
-        <div
-          className="w-20 h-20 bg-white rounded-lg flex flex-col items-center justify-center"
-          onClick={onVisitedToggle}
-        >
-          {isVisited ? (
-            <VisitedCheckbox className="p-3" />
-          ) : (
-            <UnvisitedCheckbox className="p-3" />
-          )}
-          <p className="text-center">Visited?</p>
-        </div>
+      <div
+        className="feature-card__button bottom-2 right-2"
+        onClick={onVisitedToggle}
+      >
+        {isVisited ? <VisitedCheckbox /> : <UnvisitedCheckbox />}
+        <p className="text-center">Visited?</p>
       </div>
+
       {onClose && (
         <div
-          className="absolute top-2 left-2 w-20 h-20 bg-white rounded-lg flex flex-col items-center justify-center"
+          className="feature-card__button top-2 left-2"
           onClick={onClose}
         >
           <CloseButton className="p-3" />

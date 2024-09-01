790b6f5d2423243e1bd5e355a8eae4d13369fa36
jones58 <108466184+jones58@users.noreply.github.com>
2024-09-01 13:44:35 +0100

    edit footer colour

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import VisitedCount from "../components/visitedCount.tsx";
 
 export function Footer() {
   return (
-    <div className="w-full h-[8vh] sm:h-[10vh] bg-white text-black text-3xl flex flex-col justify-center sm:justify-right space-x-4 py-[1vh] text-center sm:text-left fixed bottom-0">
+    <div className="whitebg w-full h-[8vh] sm:h-[10vh]  text-3xl flex flex-col justify-center sm:justify-right space-x-4 py-[1vh] text-center sm:text-left fixed bottom-0">
       <VisitedCount></VisitedCount>
     </div>
   );

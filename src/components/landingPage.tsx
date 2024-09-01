8e2a65eb50378df1dd929862439a6b5a8801efcf
jones58 <108466184+jones58@users.noreply.github.com>
2024-09-01 13:44:02 +0100

    edit bg colour to be default

--- src/components/landingPage.tsx
+++ src/components/landingPage.tsx
@@ -24,7 +24,7 @@ const LandingPage: React.FC = () => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-10 landing-page">
-      <div className="bg-white p-10 rounded-lg shadow-xl max-w-xl">
+      <div className=" p-10 rounded-lg shadow-xl max-w-xl">
         <h1>Welcome to the London Brutalist Map!</h1>
         <p>
           This site serves as a guide to London's Brutalist

08358f4bcdffab77996c637afe53c5c1e9bff671
jones58 <108466184+jones58@users.noreply.github.com>
2024-09-26 11:48:23 +0100

    fix footer appearance and z index

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import VisitedCount from "../components/visitedCount.tsx";
 
 export function Footer() {
   return (
-    <div className="w-full text-2xl flex flex-col justify-center sm:justify-right space-x-4 py-5 text-center sm:text-left fixed bottom-0 z-50">
+    <div className="w-full text-2xl flex flex-col justify-center sm:justify-right space-x-4 py-5 text-center sm:text-left fixed bottom-0 whitebg">
       <VisitedCount></VisitedCount>
     </div>
   );

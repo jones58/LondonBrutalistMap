e5cd4f863eec384ed6f5a077e31d5ebb9be61803
jones58 <108466184+jones58@users.noreply.github.com>
2024-09-26 12:16:33 +0100

    edit footer look

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import VisitedCount from "../components/visitedCount.tsx";
 export function Footer() {
   return (
     <div
-      className="fixed bottom-0 w-full py-5 text-2xl font-bold flex flex-col sm:flex-row items-end justify-center sm:justify-between text-center sm:text-left space-x-4 sm:pr-20 whitebg
+      className="fixed left-0 bottom-0 w-full py-5 text-2xl font-bold flex flex-col sm:flex-row justify-center sm:justify-between items-center sm:items-end text-center sm:text-left space-y-4 sm:space-y-0 sm:space-x-4 sm:px-10 whitebg
   "
     >
       <VisitedCount></VisitedCount>

9b65d78e1b55da484b8dbfa7187771cfa4544d12
jones58 <108466184+jones58@users.noreply.github.com>
2024-09-26 11:11:23 +0100

    moving to maplibre from mapbox

--- README.md
+++ README.md
@@ -33,6 +33,7 @@ Updated Brutalist Map built in React,TypeScript and Tailwind. Bootstrapped with 
 - Add color variables to SASS, to make them easier to edit.
 - Added a typography SCSS file to extract out of globals and make it easier to edit.
 - Extracted common tailwind styles to scss, to make them easier to edit.
+- Moved from Mapbox maplibre-gl, using the guides on [OpenFreeMap](https://openfreemap.org/) and their tiling. This should enable people to contribute to this repository more easily (i previously used a mapbox api key tied to the local and production servers).
 
 ## Running on your machine
 
--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,8 +1,11 @@
-import Map from "react-map-gl";
+import Map from "react-map-gl/maplibre";
+import "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
 import { FeaturesData } from "../components/geoJSONData.tsx";
 import FeatureWrapper from "../components/FeatureWrapper.tsx";
 import MapControls from "../components/mapControls.tsx";
 import LandingPage from "../components/landingPage.tsx";
+import maplibregl from "maplibre-gl";
 
 export default function MapPage() {
   const features = FeaturesData.features;
@@ -11,14 +14,14 @@ export default function MapPage() {
       <LandingPage />
       <div className="w-full h-[80vh]">
         <Map
-          mapboxAccessToken="pk.eyJ1Ijoiam9uZXM1ODEiLCJhIjoiY2xwNzAyeDVlMXlxZzJqbzVvcnBjbDZ3OCJ9.cuGt6LfugBHuqw9rJKQ2DA"
+          mapLib={maplibregl}
           initialViewState={{
             longitude: -0.1276,
             latitude: 51.5074,
             zoom: 10,
           }}
           style={{ width: "100%", height: "75vh" }}
-          mapStyle="mapbox://styles/jones581/clp8fym2g01u901qmbpzq0dde"
+          mapStyle={"./mapstyles.json"}
         >
           <MapControls />
           {features.map((feature) => (

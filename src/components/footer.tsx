515f52d6adcd3596b7837fbed4d41157712d870a
jones58 <108466184+jones58@users.noreply.github.com>
2024-09-26 12:13:35 +0100

    edit footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,8 +2,18 @@ import VisitedCount from "../components/visitedCount.tsx";
 
 export function Footer() {
   return (
-    <div className="w-full text-2xl flex flex-col justify-center sm:justify-right space-x-4 py-5 text-center sm:text-left fixed bottom-0 whitebg">
+    <div
+      className="fixed bottom-0 w-full py-5 text-2xl font-bold flex flex-col sm:flex-row items-end justify-center sm:justify-between text-center sm:text-left space-x-4 sm:pr-20 whitebg
+  "
+    >
       <VisitedCount></VisitedCount>
+      <a
+        href="https://github.com/jones58/brutalist-map-2"
+        target="_blank"
+        className="font-bold hover:underline"
+      >
+        View on Github
+      </a>
     </div>
   );
 }
